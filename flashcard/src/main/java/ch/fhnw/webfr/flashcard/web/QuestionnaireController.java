fe35bb389d6d94dfcaecf6f5521ca6c6fe979c3d
Samantha Howlett <samantha.howlett@basevision.ch>
2019-10-09 15:56:53 +0200

    AB7 - Template View

--- flashcard/src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
+++ flashcard/src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
@@ -4,6 +4,7 @@ import ch.fhnw.webfr.flashcard.domain.Questionnaire;
 import ch.fhnw.webfr.flashcard.persistence.QuestionnaireRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,39 +15,34 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.Optional;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 @Controller
 @RequestMapping("/questionnaires")
 public class QuestionnaireController {
     @Autowired
     private QuestionnaireRepository questionnaireRepository;
+    //private final Logger logger = LogManager.getLogger(QuestionnaireController.class);
 
     @RequestMapping(method = RequestMethod.GET)
-    public void findAll(HttpServletResponse response, HttpServletRequest request) throws IOException {
+    public String findAll(Model model) throws IOException {
         List<Questionnaire> questionnaires = questionnaireRepository.findAll();
-        PrintWriter writer = response.getWriter();
-        writer.append("<html><head><title>Example</title></head><body>");
-        writer.append("<h3>Fragebögen</h3>");
-        for (Questionnaire questionnaire : questionnaires) {
-            String url = request.getContextPath() + request.getServletPath();
-            url = url + "/" + questionnaire.getId().toString();
-            writer.append("<p><a href='" + response.encodeURL(url) + "'>" + questionnaire.getTitle() + "</a></p>");
-        }
-        writer.append("</body></html>");
+        model.addAttribute("questionnaires", questionnaires);
+        //logger.debug("Found " + questionnaires.size() + " questionnaires");
+        return "questionnaires/list";
     }
 
     @GetMapping(value="/{id}")
-    public void findById(@PathVariable String id, HttpServletResponse response, HttpServletRequest request) throws IOException {
+    public String findById(@PathVariable String id, Model model) throws IOException {
         Questionnaire questionnaire = questionnaireRepository.findById(id).get();
-        PrintWriter writer = response.getWriter();
-        writer.append("<html><head><title>Example</title></head><body>");
-        writer.append("<h3>Questionnaire</h3>");
-        if (questionnaire != null) {
-            writer.append("<strong>" + questionnaire.getTitle() + "</strong></br>");
-            writer.append("<span>" + questionnaire.getDescription() + "</span></p>");
+        Optional<Questionnaire> questionnaireOptional = questionnaireRepository.findById(id);
+        if(questionnaireOptional.isPresent()){
+            model.addAttribute("questionnaire", questionnaireOptional.get());
+            return "questionnaires/show";
         } else {
-            writer.append("no questionnaire found");
+            return "error";
         }
-        writer.append("</body></html>");
     }
 }
