77f551247e3432005f50aba5e6e3aa57520bead6
Samantha Howlett <samantha.howlett@basevision.ch>
2019-11-04 17:21:20 +0100

    fixed table design

--- flashcard-react-standalone/src/questionnaire/QuestionnaireTable.js
+++ flashcard-react-standalone/src/questionnaire/QuestionnaireTable.js
@@ -5,15 +5,19 @@ import { Table } from 'react-bootstrap';
 export default class QestionnaireTable extends React.Component {
 
     render() {
-        return (<Table striped hover >
+        return (<Table striped bordered hover >
+            <thead>
             <tr>
                 <td>ID</td>
                 <td>Title</td>
                 <td>Description</td>
             </tr>
+            </thead>
+            <tbody>
             {this.props.list.map((e) =>
                 <QuestionnaireTableElement element={e} />
             )}
+            </tbody>
 
         </Table>)
     }
