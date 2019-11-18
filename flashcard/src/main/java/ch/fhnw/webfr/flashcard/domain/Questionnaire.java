9faa6ab7b48baeb3dfab7c018d7efc073ba5db59
Samantha Howlett <samantha.howlett@basevision.ch>
2019-11-18 15:56:05 +0100

    init the flashcard-spring-backend

--- flashcard/src/main/java/ch/fhnw/webfr/flashcard/domain/Questionnaire.java
+++ flashcard/src/main/java/ch/fhnw/webfr/flashcard/domain/Questionnaire.java
@@ -1,30 +1,25 @@
 package ch.fhnw.webfr.flashcard.domain;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+
+@Data
 @Document(collection="questionnaires")
 public class Questionnaire {
-	@Id
-	private String id;
-	private String title;
-	private String description;
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getId() {
-		return id;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public String getDescription() {
-		return description;
-	}
+    @Id
+    private String id;
+	
+	@NotNull
+    @Size(min=2, max=30)
+    private String  title;
+	
+	@NotNull
+    @Size(min=10, max=50)
+    private String description;
+    
 }
--- flashcard/src/main/java/ch/fhnw/webfr/flashcard/persistence/QuestionnaireRepository.java
+++ flashcard/src/main/java/ch/fhnw/webfr/flashcard/persistence/QuestionnaireRepository.java
@@ -1,17 +1,11 @@
 package ch.fhnw.webfr.flashcard.persistence;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import ch.fhnw.webfr.flashcard.domain.Questionnaire;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
+import ch.fhnw.webfr.flashcard.domain.Questionnaire;
 
-public interface QuestionnaireRepository extends
-		MongoRepository<Questionnaire, String> {
+public interface QuestionnaireRepository extends MongoRepository<Questionnaire, String> {
 	List<Questionnaire> findByTitle(String title);
 }
