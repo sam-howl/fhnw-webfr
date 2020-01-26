6b6eedc4b199d1e4a63ef653e0ca757e56a76e14
Samantha Howlett <26273771+sam-howl@users.noreply.github.com>
2020-01-26 14:27:43 +0100

    rest backend - with get, create, update and delete

---
+++ flashcard-backend/src/main/java/ch/fhnw/webfr/flashcard/domain/Questionnaire.java
@@ -0,0 +1,24 @@
+package ch.fhnw.webfr.flashcard.domain;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document(collection="questionnaires")
+public class Questionnaire {
+    @Id
+    private String id;
+	
+	@NotNull
+    @Size(min=2, max=30)
+    private String  title;
+	
+	@NotNull
+    @Size(min=10, max=50)
+    private String description;
+}
